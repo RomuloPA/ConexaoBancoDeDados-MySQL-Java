8368c8fcbf5aa056930f83fbc386b4af5cccf535
Jéssica Pamplona De Almeida <romulopamplona958@gmail.com>
2023-08-10 19:59:22 -0300

    Adicionado o driver JDBC na pasta, e criado o código para realizar a conexão com o banco de dados MySQL.

--- src/App.java
+++ src/App.java
@@ -1,5 +1,19 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class App {
     public static void main(String[] args) {
-        System.out.println("Conexão com o banco de dados MySQL");
+        String jdbcUrl = "jdbc:mysql://localhost:3306/test";
+        String username = "root";
+        String password = "";
+
+        try {
+            Connection connection = DriverManager.getConnection(jdbcUrl, username, password);
+            System.out.println("Conexão bem-sucedida!");
+            connection.close();
+        } catch (SQLException e) {
+            System.err.println("Erro na conexão: " + e.getMessage());
+        }
     }
 }
