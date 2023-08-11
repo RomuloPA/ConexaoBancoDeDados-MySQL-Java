f78348d77bf08f521f06490a27598de206c0d1ef
Jéssica Pamplona De Almeida <romulopamplona958@gmail.com>
2023-08-11 15:45:07 -0300

    Criado uma tabela no banco de dados, e inserido dados através do código para testar.

--- src/App.java
+++ src/App.java
@@ -1,5 +1,6 @@
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.sql.SQLException;
 
 public class App {
@@ -8,9 +9,24 @@ public class App {
         String username = "root";
         String password = "";
 
+        String nome = "João";
+        int idade = 30;
+        String sexo = "Masculino";
+
         try {
             Connection connection = DriverManager.getConnection(jdbcUrl, username, password);
-            System.out.println("Conexão bem-sucedida!");
+
+            String insertQuery = "INSERT INTO pessoas (nome, idade, sexo) VALUES (?, ?, ?)";
+            PreparedStatement preparedStatement = connection.prepareStatement(insertQuery);
+            preparedStatement.setString(1, nome);
+            preparedStatement.setInt(2, idade);
+            preparedStatement.setString(3, sexo);
+
+            int rowsInserted = preparedStatement.executeUpdate();
+            if (rowsInserted > 0) {
+                System.out.println("Pessoa inserida com sucesso!");
+            }
+
             connection.close();
         } catch (SQLException e) {
             System.err.println("Erro na conexão: " + e.getMessage());
