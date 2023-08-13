b5c3a50609a4c3a89c3256c8942ebd06c70d38c5
Jéssica Pamplona De Almeida <romulopamplona958@gmail.com>
2023-08-12 23:42:04 -0300

    Alterado o código para que as informações das pessoas sejam inseridas no banco de dados através de entradas digitadas pelo usuário.

--- src/App.java
+++ src/App.java
@@ -2,6 +2,7 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
+import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) {
@@ -9,12 +10,20 @@ public class App {
         String username = "root";
         String password = "";
 
-        String nome = "João";
-        int idade = 30;
-        String sexo = "Masculino";
-
         try {
             Connection connection = DriverManager.getConnection(jdbcUrl, username, password);
+
+            Scanner scanner = new Scanner(System.in);
+
+            System.out.print("Digite o nome da pessoa: ");
+            String nome = scanner.nextLine();
+
+            System.out.print("Digite a idade da pessoa: ");
+            int idade = scanner.nextInt();
+            scanner.nextLine(); // Consumir a nova linha pendente após a entrada numérica
+
+            System.out.print("Digite o sexo da pessoa: ");
+            String sexo = scanner.nextLine();
 
             String insertQuery = "INSERT INTO pessoas (nome, idade, sexo) VALUES (?, ?, ?)";
             PreparedStatement preparedStatement = connection.prepareStatement(insertQuery);
@@ -27,7 +36,9 @@ public class App {
                 System.out.println("Pessoa inserida com sucesso!");
             }
 
+            preparedStatement.close();
             connection.close();
+            scanner.close();
         } catch (SQLException e) {
             System.err.println("Erro na conexão: " + e.getMessage());
         }
