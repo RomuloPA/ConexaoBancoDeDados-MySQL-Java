18dbfdbb55998c35a7d1efdce17d5803fd3847f2
Jéssica Pamplona De Almeida <romulopamplona958@gmail.com>
2023-08-13 23:24:48 -0300

    Adicionado a funcionalidade de exclusão de pessoa do banco de dados.

--- src/App.java
+++ src/App.java
@@ -1,6 +1,7 @@
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.Scanner;
 
@@ -15,28 +16,63 @@ public class App {
 
             Scanner scanner = new Scanner(System.in);
 
-            System.out.print("Digite o nome da pessoa: ");
-            String nome = scanner.nextLine();
+            System.out.println("Escolha uma opção:");
+            System.out.println("1 - Inserir pessoa");
+            System.out.println("2 - Excluir pessoa");
+            int choice = scanner.nextInt();
+            scanner.nextLine();
 
-            System.out.print("Digite a idade da pessoa: ");
-            int idade = scanner.nextInt();
-            scanner.nextLine(); // Consumir a nova linha pendente após a entrada numérica
+            if (choice == 1) {
+                System.out.print("Digite o nome da pessoa: ");
+                String nome = scanner.nextLine();
 
-            System.out.print("Digite o sexo da pessoa: ");
-            String sexo = scanner.nextLine();
+                System.out.print("Digite a idade da pessoa: ");
+                int idade = scanner.nextInt();
+                scanner.nextLine();
 
-            String insertQuery = "INSERT INTO pessoas (nome, idade, sexo) VALUES (?, ?, ?)";
-            PreparedStatement preparedStatement = connection.prepareStatement(insertQuery);
-            preparedStatement.setString(1, nome);
-            preparedStatement.setInt(2, idade);
-            preparedStatement.setString(3, sexo);
+                System.out.print("Digite o sexo da pessoa: ");
+                String sexo = scanner.nextLine();
 
-            int rowsInserted = preparedStatement.executeUpdate();
-            if (rowsInserted > 0) {
-                System.out.println("Pessoa inserida com sucesso!");
+                String insertQuery = "INSERT INTO pessoas (nome, idade, sexo) VALUES (?, ?, ?)";
+                PreparedStatement preparedStatement = connection.prepareStatement(insertQuery);
+                preparedStatement.setString(1, nome);
+                preparedStatement.setInt(2, idade);
+                preparedStatement.setString(3, sexo);
+
+                int rowsInserted = preparedStatement.executeUpdate();
+                if (rowsInserted > 0) {
+                    System.out.println("Pessoa inserida com sucesso!");
+                }
+
+                preparedStatement.close();
+            } else if (choice == 2) {
+                String selectQuery = "SELECT id, nome, idade, sexo FROM pessoas";
+                PreparedStatement selectStatement = connection.prepareStatement(selectQuery);
+                ResultSet resultSet = selectStatement.executeQuery();
+
+                System.out.println("Lista de pessoas cadastradas:");
+                while (resultSet.next()) {
+                    int id = resultSet.getInt("id");
+                    String nome = resultSet.getString("nome");
+                    int idade = resultSet.getInt("idade");
+                    String sexo = resultSet.getString("sexo");
+                    System.out.println("ID: " + id + ", Nome: " + nome + ", Idade: " + idade + ", Sexo: " + sexo);
+                }
+
+                System.out.print("Digite o ID da pessoa que deseja excluir: ");
+                int idToDelete = scanner.nextInt();
+
+                String deleteQuery = "DELETE FROM pessoas WHERE id = ?";
+                PreparedStatement deleteStatement = connection.prepareStatement(deleteQuery);
+                deleteStatement.setInt(1, idToDelete);
+                int rowsDeleted = deleteStatement.executeUpdate();
+                if (rowsDeleted > 0) {
+                    System.out.println("Pessoa excluída com sucesso!");
+                }
+
+                deleteStatement.close();
             }
 
-            preparedStatement.close();
             connection.close();
             scanner.close();
         } catch (SQLException e) {
